d33a7259dc8701617c2fbbdd2d550cdd68bb1537
cazjonoli <cazjonoli@gmail.com>
2018-08-12 23:28:55 +0000

    Added profile edit action and view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -20,6 +20,12 @@ class ProfilesController < ApplicationController
     end
   end
   
+  # GET to '/users/:user_id/profile/edit'
+  def edit
+    @user = User.find(params[:user_id])
+    @profile = @user.profile
+  end
+  
   private
   # Whitelisting
     def profile_params
