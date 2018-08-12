3e76ee9b29c36ae64c3f933c07823eab7e57d0cb
cazjonoli <cazjonoli@gmail.com>
2018-08-12 19:31:24 +0000

    Added code comments for Stripe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   
-  
+  # Whitelist the following form fields so that we can process them, if
+  # coming from a Devise sign up form.
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   protected
